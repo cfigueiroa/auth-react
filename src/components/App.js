8ca50039c218789604b085be571b3b8428bb41be
cfigueiroa <cfigueiroa@gmail.com>
2022-12-10 00:39:52 -0300

    finish

--- src/components/App.js
+++ src/components/App.js
@@ -11,14 +11,38 @@ import Title from "./Title";
 export default function App() {
   const [list, setList] = useState([]);
   const [token, setToken] = useState("");
+  const [form, setForm] = useState({ email: "", password: "" });
 
-  function login() {
+  function login(e) {
     // faça o login e armazene o token no estado token
+    // bootcamper@respondeai.com.br
+    // minha_senha_super_hiper_ultra_secreta_banana
+    e.preventDefault();
+    const URL = "https://mock-api.bootcamp.respondeai.com.br/api/v2/exercise-login/auth/login";
+    const promise = axios.post(URL, form);
+    promise.then(res => setToken(res.data.token));
+    promise.catch(err => console.log(`login catch: ${err.response.data}`));
   }
 
   function fetchList() {
     // busque a lista enviando o token no cabeçalho
     // e coloque-a no estado list
+    const URL = "https://mock-api.bootcamp.respondeai.com.br/api/v2/exercise-login/tasks";
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    };
+    const promise = axios.get(URL, config);
+    promise.then(res => setList(res.data));
+    promise.catch(err => console.log(`fetchList catch: ${err.response.data}`));
+  }
+
+  function handleForm(e) {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value
+    })
   }
 
   return (
@@ -28,9 +52,11 @@ export default function App() {
         <br />
         Shopping List
       </Title>
-      <Input type="text" placeholder="Digite seu email..." />
-      <Input type="text" placeholder="Digite sua senha..." />
-      <Button onClick={login}>Entrar</Button>
+      <form onSubmit={login}>
+        <Input name="email" onChange={handleForm} value={form.email} type="email" placeholder="Digite seu email..." />
+        <Input name="password" onChange={handleForm} value={form.password} type="password" placeholder="Digite sua senha..." />
+        <Button>Entrar</Button>
+      </form>
       {token ? (
         <Button onClick={fetchList}>Buscar lista de compras</Button>
       ) : (
@@ -38,7 +64,7 @@ export default function App() {
       )}
       <List>
         {list.map((i) => (
-          <Item>{i}</Item>
+          <Item key={i}>{i}</Item>
         ))}
       </List>
     </Page>
